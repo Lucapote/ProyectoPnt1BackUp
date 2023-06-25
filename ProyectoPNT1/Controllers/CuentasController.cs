ec3bc87abbfe9bba41b8a0da857ec109e380b5e9
ASUS <ASUS@DESKTOP-3CFN9QQ>
2023-06-24 23:54:19 -0300

    antes de modificar la creacion y edicion

---
+++ ProyectoPNT1/Controllers/CuentasController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoPNT1.Models;
+using ProyectoPNT1.Recursos;
+using ProyectoPNT1.ViewModel;
+
+namespace ProyectoPNT1.Controllers
+{
+    public class CuentasController : Controller
+    {
+        private readonly SignInManager<Persona> _signInManager;
+
+        public CuentasController(SignInManager<Persona> signInManager)
+        {
+            this._signInManager = signInManager;
+        }
+        public IActionResult IniciarSesion()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> IniciarSesion(Login viewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            var resultado =
+                await _signInManager.PasswordSignInAsync(viewModel.Email, viewModel.Password, viewModel.RememberMe, lockoutOnFailure: false);
+            if(resultado.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, ErrorMsg.ErrorLogin);
+                return View(viewModel);
+            }
+        }
+    }
+}
---
+++ ProyectoPNT1/ViewModel/Login.cs
@@ -0,0 +1,20 @@
+﻿using ProyectoPNT1.Recursos;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoPNT1.ViewModel
+{
+    public class Login
+    {
+        [Required(ErrorMessage = ErrorMsg.Required)]
+        [EmailAddress(ErrorMessage = ErrorMsg.Email)]
+        [Display(Name = Alias.Email)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = ErrorMsg.Required)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = Alias.RememberMe)]
+        public bool RememberMe { get; set; }
+    }
+}

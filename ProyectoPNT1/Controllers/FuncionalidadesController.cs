cd18268962e596c1e0a175275e1b991faa029fa7
ASUS <ASUS@DESKTOP-3CFN9QQ>
2023-07-05 22:07:12 -0300

    Calcular Horas

---
+++ ProyectoPNT1/Controllers/FuncionalidadesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoPNT1.Data;
+using ProyectoPNT1.ViewModel;
+
+public class FuncionalidadesController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public FuncionalidadesController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult CalcularTotalHoras()
+    {
+        //Creamos la instancia del ViewModel que se va a mostrar
+        var viewModel = new TotalHoras();
+
+        return View(viewModel);
+    }
+
+
+    [HttpPost]
+    public IActionResult CalcularTotalHoras(TotalHoras viewModel)
+    {
+        if (ModelState.IsValid)
+        {
+            DateTime fechaInicio = viewModel.FechaInicio.Date;
+            DateTime fechaFin = viewModel.FechaFin.Date.AddDays(1); // Incluir la fecha final en el rango
+
+            var horarios = _context.Horario
+                .Where(h => h.FechaTrabajo >= fechaInicio && h.FechaTrabajo < fechaFin)
+                .ToList(); // Traer los registros a memoria
+
+            double totalHoras = horarios
+                .Sum(h => h.HoraSalida.HasValue && h.HoraEntrada.HasValue ? (h.HoraSalida.Value - h.HoraEntrada.Value).TotalHours : 0);
+
+
+            viewModel.TotalHorasMostrar = totalHoras;
+        }
+
+        return View(viewModel);
+    }
+}
---
+++ ProyectoPNT1/ViewModel/TotalHoras.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoPNT1.ViewModel
+{
+    public class TotalHoras
+    {
+        public TotalHoras()
+        {
+        // Obtener el primer y último día de la semana actual
+        var primerDiaSemana = DateTime.Now.Date.AddDays(-(int)DateTime.Now.DayOfWeek);
+        var ultimoDiaSemana = primerDiaSemana.AddDays(6);
+
+        // Asignar los valores predeterminados
+        FechaInicio = primerDiaSemana;
+        FechaFin = ultimoDiaSemana;
+        }
+
+        [Required(ErrorMessage = "La fecha de inicio es obligatoria.")]
+        [Display(Name = "Fecha de inicio")]
+        public DateTime FechaInicio { get; set; }
+
+        [Required(ErrorMessage = "La fecha de fin es obligatoria.")]
+        [Display(Name = "Fecha de fin")]
+        public DateTime FechaFin { get; set; }
+
+        public double TotalHorasMostrar { get; set; }
+    }
+}
